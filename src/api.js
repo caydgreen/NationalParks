50cd05c7d8d15ed540f12c1332061629d6cfd5f7
Cayla Green <cayladawngreen@gmail.com>
2019-01-29 12:32:25 -0600

    Setup API and axios

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// set base URL to setup axios
+axios.defaults.baseURL = 'https://developer.nps.gov/api/v1';
+axios.defaults.params = {
+  api_key: process.env.VUE_APP_API_KEY,
+};
+
+export default{
+
+};
